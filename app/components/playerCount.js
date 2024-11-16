4b5ab5124163dacdd132278875c30c55671c3afa
Muhammad Yusuf Habibie <myh481813@gmail.com>
2024-11-16 12:24:49 +0700

    add: hooks on countPlayer and recentWinner

--- app/components/playerCount.js
+++ app/components/playerCount.js
@@ -1,42 +1,22 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { readContract } from "@wagmi/core";
-import { config } from "@/app/lib/config";
 import { abi, contractAddress } from "@/app/lib/abi";
+import { useReadContract } from 'wagmi'
 
 export default function PlayerCount() {
-  const [count, setCount] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchCountPlayer = async () => {
-      try {
-        const result = await readContract(config, {
-          abi,
-          address: contractAddress,
-          functionName: "getNumberOfPlayers",
-        });
-        setCount(result.toString());
-      } catch (error) {
-        console.error("Error fetching recent count:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchCountPlayer();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { data: count, isPending } = useReadContract({
+    abi,
+    address: contractAddress,
+    functionName: "getNumberOfPlayers",
+  });
 
   return (
     <div className="text-sm xl:text-lg text-center overflow-hidden rounded-xl px-3 py-1 bg-blue-400 mb-2 text-white lg:fixed lg:bottom-20 ">
-      {isLoading ? (
+      {isPending ? (
         <p>Player Joined: Loading...</p>
-      ) : count ? (
-        <p className="">Player Joined: {count}</p>
       ) : (
-        <p>Player Joined: Not available</p>
+        <p className="">Player Joined: {count.toString()}</p>
       )}
     </div>
   );
--- app/components/recentWinner.js
+++ app/components/recentWinner.js
@@ -4,44 +4,27 @@ import { useState, useEffect } from "react";
 import { readContract } from "@wagmi/core";
 import { config } from "@/app/lib/config";
 import { abi, contractAddress } from "@/app/lib/abi";
+import { useReadContract } from 'wagmi'
 
 export default function RecentWinner() {
-  const [winner, setWinner] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
 
   function shortenAddress(address, chars = 4) {
     return `${address.slice(0, chars + 2)}…${address.slice(-chars)}`;
   }
 
-  useEffect(() => {
+  const { data: winner, isPending } = useReadContract({
+    abi,
+    address: contractAddress,
+    functionName: "getRecentWinner",
+  });
 
-    const fetchWinner = async () => {
-      try {
-        const result = await readContract(config, {
-          abi,
-          address: contractAddress,
-          functionName: "getRecentWinner",
-        });
-        setWinner(shortenAddress(result));
-      } catch (error) {
-        console.error("Error fetching recent winner:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchWinner();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <div className="text-white text-sm xl:text-lg text-center overflow-hidden rounded-2xl py-2 px-3 bg-green-400 lg:fixed lg:bottom-10 xl:bottom-8">
-      {isLoading ? (
+      {isPending ? (
         <p>Recent Winner: Loading...</p>
-      ) : winner ? (
-        <p className="">Recent Winner: {winner}</p>
       ) : (
-        <p>Recent Winner: Not available</p>
+        <p className="">Recent Winner: {shortenAddress(winner)}</p>
       )}
     </div>
   );
